7fe420e4bb23dde3ac1ce198e752ba7a1c38d93d
Richard1DL <163780259+Richard1DL@users.noreply.github.com>
2025-05-31 22:38:29 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('form');
-const btn = document.getElementById('button');
-
-form.addEventListener('submit', () => {
-  btn.value = 'Enviando...';
-  btn.disabled = true;
-});
